ff1521411be4ceec780b2d0b76ac4c6aa53c7de0
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2024-08-26 00:42:56 +0100

    Update bucket.tf

--- Eks-Terraform/bucket.tf
+++ Eks-Terraform/bucket.tf
@@ -19,3 +19,18 @@ status = "Enabled"
  }
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "manoj.tertis.game2024"
+    region         = "ap-south-1"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Eks-Terraform/terraform.tfstate"
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
